276d966a39debcf117748d6002069a63ccc16bb0
Jorge Simao <jsimao71@gmail.com>
2026-08-11 20:57:23 +0100

    feat: add recall sparsity metrics

--- src/common/__init__.py
+++ src/common/__init__.py
@@ -1,6 +1,7 @@
 """Reusable infrastructure for Transformer research experiments."""
 
 from .config import TrainConfig, deep_update, load_yaml_config, read_yaml
+from .recall_sparsity import DEFAULT_FRACTIONS, recall_sparsity_curve
 from .train import (
     TrainingState,
     create_training_state,
@@ -13,12 +14,14 @@ from .train import (
 __all__ = [
     "TrainConfig",
     "TrainingState",
+    "DEFAULT_FRACTIONS",
     "create_training_state",
     "deep_update",
     "default_batch_step",
     "load_yaml_config",
     "move_batch",
     "read_yaml",
+    "recall_sparsity_curve",
     "resolve_device",
     "train_model",
 ]
